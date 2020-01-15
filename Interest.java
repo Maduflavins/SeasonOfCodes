a7d531080cc0bb7293a4a4a8eec05bb35fb3fe1d
Ayemobola Tolulope <ayemobolatolulope@gmail.com>
2020-01-15 14:16:20 +0100

    Newest commit

---
+++ Interest.java
@@ -0,0 +1,16 @@
+public class Interest{
+	public static void main (String[] args){
+		
+		double amount;
+		double principal = 1000.0;
+		double rate = 0.05;
+
+		System.out.printf("%s%20s%n", "Year", "Deposit Amount");
+
+		for (int year = 1; year <= 10; ++year){
+			amount = principal * Math.pow(1.0 + rate, year);
+			// display the year and the amount
+			System.out.printf("%2d%,20.2f%n", year, amount);
+		}
+	}
+}
